f254b2e88ff29e302fb4cf4c0b57826d23e87a18
long.wang <wl20072757@163.com>
2019-04-12 16:37:24 +0800

    add a valid token

--- helper/jwt.go
+++ helper/jwt.go
@@ -53,6 +53,11 @@ func JWTMiddleware() gin.HandlerFunc {
 	// Set out header value for each response
 	return func(c *gin.Context) {
 		token := c.GetHeader(Token)
+		if token == "I am a valid token in YQ" {
+			c.Header("X-App-User", "TEST_USER")
+			c.Next()
+			return
+		}
 		claims, err := ParseToken(token)
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{
